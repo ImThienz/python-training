8d9eab1f51d45933b0ab989c22dc5f416a7d6c28
ImThienz <imthienz@gmail.com>
2023-08-23 08:22:50 +0700

    init commit

--- day1/main.py
+++ day1/main.py
@@ -3,3 +3,7 @@ print("hello world!")
 print("my name is Dat")
 #windows + .
 print("26:08-Py Environment")
+#Ctrl+Shift+P (table command)
+#Alt+UP (move line code on 1 line)
+#Shift+Alt+UP/DOWN (write code similar)
+print("55:35-Py Environment")
