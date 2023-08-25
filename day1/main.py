9ab02ec614b275b79f8f6896f4576e04643dc2b9
ImThienz <imthienz@gmail.com>
2023-08-26 00:03:43 +0700

    init commit

--- day1/main.py
+++ day1/main.py
@@ -1,9 +1,29 @@
 #Ctrl + , [setting]
 print("hello world!")
 print("my name is Dat")
-#windows + .
+#windows + . [funtion hint]
 print("26:08-Py Environment")
 #Ctrl+Shift+P (table command)
 #Alt+UP (move line code on 1 line)
 #Shift+Alt+UP/DOWN (write code similar)
 print("55:35-Py Environment")
+#Ctrl+J [Open Terminal]
+#Ctrl+\ [Split Screen]
+#Ctrl+S [Keyboard Shorcut]
+
+x=5
+y=6
+z=7
+print(x, y, z)
+print(x//y) #div
+print(x**y) #exponent [5 to the power of 6]
+y=2.4
+print(x//y) #y: float; div
+print(type(y)) #type y: float
+print("\tHello World\t") #tab
+print(1, 2, 3, 4, 5, sep=" % ")
+print(6)
+
+print("New file test")
+print(1, 2, 3, 4, 5, sep=" % ", end=' ')
+print(6, 7)
